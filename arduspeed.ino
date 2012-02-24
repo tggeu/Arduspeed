c38d0f2fe3acd575a353b5ecc2fcad11c86a1330
sprae <saperwl@o2.pl>
2012-02-24 21:23:58 +0100

    Added LCD display

--- arduspeed.ino
+++ arduspeed.ino
@@ -1,16 +1,28 @@
 #include <Messenger.h>
 #include <Servo.h>
+#include <LiquidCrystal.h>
 
 #define SPEED_SERVO_PIN 2
 #define SPEED_SERVO_0_US 2800
 #define SPEED_SERVO_200_US 870
 
+#define LCD_RS_PIN 6
+#define LCD_E_PIN 7
+#define LCD_D4_PIN 8
+#define LCD_D5_PIN 9
+#define LCD_D6_PIN 10
+#define LCD_D7_PIN 11
+#define LCD_LINES 2
+#define LCD_ROWS 16
+
 Servo hand;
 Messenger message;
+LiquidCrystal lcd(LCD_RS_PIN, LCD_E_PIN, LCD_D4_PIN,
+                  LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
 void sendVersion()
 {
-  Serial.println(F("AD 01.12 ArduDash"));
+  Serial.println(F("AD 02.12 ArduSpeed"));
 }
 
 void sendOK()
@@ -21,6 +33,33 @@ void sendOK()
 void sendERROR()
 {
   Serial.println(F("ERROR"));
+}
+
+void lcd_rjust_number(int num, byte length)
+{
+  if (num < 10)
+  {
+    lcd.print(F(" "));
+  }
+
+  if (num < 100)
+  {
+    lcd.print(F(" "));
+  }
+
+  if (num < 1000 && length == 4)
+  {
+    lcd.print(F(" "));
+  }
+
+  lcd.print(num);
+}
+
+void setSpeedLCD(int speed)
+{
+  lcd.setCursor(0, 0);
+  lcd_rjust_number(speed, 3);
+  lcd.print(F(" km/h"));
 }
 
 void setSpeedServo(int speed)
@@ -37,7 +76,112 @@ void setSpeed()
     sendERROR();
     return;
   }
+
   setSpeedServo(speed);
+  setSpeedLCD(speed);
+  sendOK();
+}
+
+void setFuelLCD(float fuel)
+{
+  lcd.setCursor(11, 0);
+  if (fuel >= 100.0)
+  {
+    lcd.print(F(" "));
+    lcd.print(fuel, 0);
+  }
+
+  else if (fuel >= 10.0)
+  {
+    lcd.print(fuel, 1);
+  }
+
+  else
+  {
+    lcd.print(fuel, 2);
+  }
+
+  lcd.print(F("%"));
+}
+
+void setFuel()
+{
+  int fuel_int = message.readInt();
+  if (fuel_int > 10000 || fuel_int < 0)
+  {
+    sendERROR();
+    return;
+  }
+
+  float fuel = fuel_int / 100.0;
+
+  setFuelLCD(fuel);
+  sendOK();
+}
+
+void setGearLCD(int gear)
+{
+  lcd.setCursor(9, 0);
+  if (gear == 0)
+  {
+    lcd.print(F("R"));
+  }
+  
+  else if (gear == 1)
+  {
+    lcd.print(F("N"));
+  }
+  
+  else
+  {
+    lcd.print(gear - 1);
+  }
+}
+
+void setGear()
+{
+  int gear = message.readInt();
+  if (gear > 7 || gear < 0)
+  {
+    sendERROR();
+    return;
+  }
+
+  setGearLCD(gear);
+  sendOK();
+}
+
+void setRPMLCD(int rpm)
+{
+  lcd.setCursor(0, 1);
+  byte progress_width = map(rpm, 0, 10000, 0, 10);
+  for (byte x=0; x<10; x++)
+  {
+    if (x < progress_width)
+    {
+      lcd.write((byte) 1);
+    }
+
+    else
+    {
+      lcd.write((byte) 0);
+    }
+  }
+
+  lcd.print(" ");
+  lcd_rjust_number(rpm, 4);
+}
+
+void setRPM()
+{
+  int rpm = message.readInt();
+  if (rpm > 10000 || rpm < 0)
+  {
+    sendERROR();
+    return;
+  }
+
+  setRPMLCD(rpm);
   sendOK();
 }
 
@@ -48,13 +192,55 @@ void onMessage()
     case 'V':
       sendVersion();
       break;
+
     case 'S':
       setSpeed();
       break;
+
+    case 'F':
+      setFuel();
+      break;
+
+    case 'G':
+      setGear();
+      break;
+
+    case 'R':
+      setRPM();
+      break;
+
     default:
       sendERROR();
       break;
   }
+}
+
+void createChars()
+{
+  byte circle_open[8] = {
+    B01110,
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B01110,
+    B00000
+  };
+
+  byte circle_close[8] = {
+    B01110,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B01110,
+    B00000
+  };
+
+  lcd.createChar(0, circle_open);
+  lcd.createChar(1, circle_close);
 }
 
 void serialEvent()
@@ -67,7 +253,9 @@ void setup()
   hand.attach(2);
   Serial.begin(19200);
   message.attach(onMessage);
-  
+  lcd.begin(LCD_ROWS, LCD_LINES);
+  createChars();
+
   setSpeedServo(0);
 }
 
--- arduspeed.py
+++ arduspeed.py
@@ -10,7 +10,19 @@ time.sleep(3)
 def outgauge_packet(outgauge, packet):
   speed_kmh = int(packet.Speed*3.6)
   speed_command = 'S {0}\r'.format(speed_kmh)
-  arduino.write(speed_command)
+
+  fuel_int = int(packet.Fuel * 100 * 100)
+  fuel_commend = 'F {0}\r'.format(fuel_int)
+
+  gear = packet.Gear
+  gear_command = 'G {0}\r'.format(gear)
+
+  rpm = int(round(packet.RPM, -2))
+  rpm_command = 'R {0}\r'.format(rpm)
+
+  command = speed_command + fuel_commend + gear_command + rpm_command
+
+  arduino.write(command)
   arduino.flushInput()
 
 
